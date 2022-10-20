075e9abff849b2131a008ebe58b7d2a0518f1e9f
Victor MASIAK <victor.masiak@gmail.com>
2022-10-20 16:10:22 +0800

    conflict resolve

--- research/mi_lira_2021/scripts/all.sh
+++ research/mi_lira_2021/scripts/all.sh
@@ -1,4 +1,7 @@
+<<<<<<< HEAD
+=======
 bash scripts/train_large.sh
+>>>>>>> fe0d283a80e4f34ebd28465882cd468b441ea405
 python inference.py --logdir=exp/cifar10/
 python score.py exp/cifar10/
 python generate_embeddings.py --logdir=exp/cifar10/
